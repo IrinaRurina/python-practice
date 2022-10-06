bb0ac9d0c52309291068eade4ac947651fb8499a
IrinaRurina <irina.rurina@gmail.com>
2022-10-06 15:21:46 +0300

    log_upd

--- practice9-2/transform_num.py
+++ practice9-2/transform_num.py
@@ -1,14 +1,13 @@
 from fractions import Fraction
 
 
-def transform_numbers(num_type, value1, value2):
+def transform_complex(value1, value2):
+    value1 = complex(value1)
+    value2 = complex(value2)
+    return value1, value2
 
-    if num_type == '1':
-        value1 = complex(value1)
-        value2 = complex(value2)
 
-    elif num_type == '2':
-        value1 = Fraction(int(value1[0: value1.index('/')]), int(value1[value1.index('/')+1:len(value1)]))
-        value2 = Fraction(int(value2[0: value2.index('/')]), int(value2[value2.index('/')+1:len(value2)]))
-
+def transform_rational(value1, value2):
+    value1 = Fraction(int(value1[0: value1.index('/')]), int(value1[value1.index('/')+1:len(value1)]))
+    value2 = Fraction(int(value2[0: value2.index('/')]), int(value2[value2.index('/')+1:len(value2)]))
     return value1, value2
